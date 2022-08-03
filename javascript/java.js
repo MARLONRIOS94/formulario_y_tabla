a2ca993a9fb3b614b74e98cf87a5b9c9a23bf359
Marlon Rios <marlonrios1994@gmail.com>
2022-08-03 16:26:42 -0500

    inicio de configuracion del local storage

---
+++ javascript/java.js
@@ -0,0 +1,95 @@
+// Evento que se ejecuta al pulsar el botón agregar
+document.querySelector("input[type=submit]").addEventListener("click",function(e){
+    // cancelamos el evento
+    e.preventDefault();
+ 
+    const nombre=document.querySelector("input[name=nombre]");
+    const apellido=document.querySelector("input[name=apellido]");
+    const telefono=document.querySelector("input[name=telefono]");
+    const correo=document.querySelector("input[name=correo]");
+
+
+    if (!nombre.value) {
+        nombre.classList.add("error");
+        alert("Ingrese el nombre")
+        return;
+    }
+    if (!apellido.value) {
+        nombre.classList.add("error");
+        alert("Ingrese el apellido")
+        return;
+    }
+    if (!telefono.value) {
+        nombre.classList.add("error");
+        alert("Ingrese el telefono")
+        return;
+    }
+    if (!correo.value) {
+        nombre.classList.add("error");
+        alert("Ingrese el correo")
+        return;
+    }
+
+
+    agregarFila(nombre.value, apellido.value,telefono.value,correo.value);
+ 
+    //limpiamos los valores del input
+    correo.value = "";
+    telefono.value ="";
+    apellido.value ="";
+    nombre.value ="";
+    nombre.focus();
+ 
+});
+ 
+
+function agregarFila(nombre, apellido,telefono,correo) {
+    // añadimos el alumno a la tabla crando el tr, td's y el botón para eliminarlo
+    const tr=document.createElement("tr");
+ 
+    const tdNombre=document.createElement("td");
+    let txt=document.createTextNode(nombre);
+    tdNombre.appendChild(txt);
+    tdNombre.className="nombre";
+ 
+    const tdApellido=document.createElement("td");
+    txt=document.createTextNode(apellido);
+    tdApellido.appendChild(txt);
+    tdApellido.className="apellido";
+
+    const tdTelefono=document.createElement("td");
+    txt=document.createTextNode(telefono);
+    tdTelefono.appendChild(txt);
+    tdTelefono.className="telefono";
+
+    const tdCorreo=document.createElement("td");
+    txt=document.createTextNode(correo);
+    tdCorreo.appendChild(txt);
+    tdCorreo.className="correo";
+
+    const tdRemove=document.createElement("td");
+    const buttonRemove=document.createElement("img");
+    buttonRemove.src="../imagenes/eliminar.png";
+    buttonRemove.onclick=eliminarFila;
+    tdRemove.appendChild(buttonRemove);
+
+    tr.appendChild(tdNombre);
+    tr.appendChild(tdApellido);
+    tr.appendChild(tdTelefono);
+    tr.appendChild(tdCorreo);
+    tr.appendChild(tdRemove);
+
+    const tbody=document.getElementById("listado").querySelector("tbody").appendChild(tr);
+
+}
+
+
+function eliminarFila(e) {
+
+    this.closest("tr").remove();
+
+    if (document.getElementById("listado").querySelector("tbody").querySelectorAll("tr").length==0) {
+        document.getElementById("listado").classList.add("hide");
+    }
+ 
+}
